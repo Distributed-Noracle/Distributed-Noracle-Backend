414444eb65ac3027964df20ca7cd419a491a9e60
Peter de Lange <lange@dbis.rwth-aachen.de>
2017-10-02 16:55:30 +0200

    question-length now random

--- noracle-generate-examples.sh
+++ noracle-generate-examples.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-ENDPOINT="https://steen.informatik.rwth-aachen.de:9082/distributed-noracle/v0.5.0"
+ENDPOINT="https://localhost:9082/distributed-noracle/v0.5.0"
 
 SPACE_CREATOR_AGENT_ID="762a164ba8eb06d3d110eeeb0321cb75f2624ab311f9c0c4d0e91bdc854a02f389b91573d6c29d70d3071a8a1738cd20fac87d638777dbfcef4ded8be71e2656"
 SPACE_CREATOR_AGENT_LOGIN="noracle-example-smith"
 
 AGENT_PW="testtest"
 
-QUESTION_TEXT="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi."
+random_lengthLorem() {
+  #1703 chars
+  QUESTION_TEXT="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi."
+  # should be possible to at least print 200 chars
+  start=$((RANDOM % ((${#QUESTION_TEXT} - (RANDOM % 1500)))))
+  # max 500 chars, not more then avaible after starting position
+  printf '%s' "${QUESTION_TEXT:$start:(RANDOM % (500 % (1703-$start)))}"
+}
 
 # create example spaces
 
@@ -20,8 +27,7 @@ done
 
 curl -s --user noracle-example-smith:${AGENT_PW} -X GET --header 'Accept: application/json' "${ENDPOINT}"'/agents/'"${SPACE_CREATOR_AGENT_ID}"'/spacesubscriptions' --insecure | jq -r '.[].spaceId' | while read spaceId ; do
   for num in {1..50}; do
-    curl -s -D - --user noracle-example-smith:${AGENT_PW} -X POST --header 'Content-Type: application/json' -d '{ "text": "'"${QUESTION_TEXT}"'" }' "${ENDPOINT}"'/spaces/'"${spaceId}"'/questions' --insecure
+    random=$(random_lengthLorem)
+    curl -s -D - --user noracle-example-smith:${AGENT_PW} -X POST --header 'Content-Type: application/json' -d '{ "text": "'"${random}"'" }' "${ENDPOINT}"'/spaces/'"${spaceId}"'/questions' --insecure
   done
 done
-
-
