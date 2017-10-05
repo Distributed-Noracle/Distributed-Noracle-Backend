84ae301bb550c06034355c90af68ca745cdd11ce
Thomas Cujé <tcuje@freenet.de>
2017-10-05 09:41:32 +0200

    add relations to example data generator script

--- noracle-generate-examples.sh
+++ noracle-generate-examples.sh
@@ -1,33 +1,59 @@
 #!/bin/bash
 
-ENDPOINT="https://steen.informatik.rwth-aachen.de:9082/distributed-noracle/v0.5.0"
+endpoint="https://steen.informatik.rwth-aachen.de:9082/distributed-noracle/v0.5.0"
 
-SPACE_CREATOR_AGENT_ID="762a164ba8eb06d3d110eeeb0321cb75f2624ab311f9c0c4d0e91bdc854a02f389b91573d6c29d70d3071a8a1738cd20fac87d638777dbfcef4ded8be71e2656"
-SPACE_CREATOR_AGENT_LOGIN="noracle-example-smith"
+space_creator_agent_id="762a164ba8eb06d3d110eeeb0321cb75f2624ab311f9c0c4d0e91bdc854a02f389b91573d6c29d70d3071a8a1738cd20fac87d638777dbfcef4ded8be71e2656"
+space_creator_agent_login="noracle-example-smith"
 
-AGENT_PW="testtest"
+agent_pw="testtest"
+
+num_spaces=5
+num_questions_per_space=50
+num_relations_per_space=25
+
+relation_types=()
+relation_types+=("\"name\": \"Similarity\", \"directed\": \"false\"")
+relation_types+=("\"name\": \"FollowUp\", \"directed\": \"true\"")
+relation_types+=("\"name\": \"LinksWith\", \"directed\": \"false\"")
 
 random_lengthLorem() {
   #1703 chars
-  QUESTION_TEXT="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi."
+  question_text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi."
   # should be possible to at least print 200 chars
-  start=$((RANDOM % ((${#QUESTION_TEXT} - (RANDOM % 1500)))))
+  start=$((RANDOM % ((${#question_text} - (RANDOM % 1500)))))
   # max 500 chars, not more then avaible after starting position
-  printf '%s' "${QUESTION_TEXT:$start:(RANDOM % (500 % (1703-$start)))}"
+  printf '%s' "${question_text:$start:(RANDOM % (500 % (1703-$start)))}"
 }
 
 # create example spaces
 
-for num in {1..5}; do
-  curl -s -D - --user noracle-example-smith:${AGENT_PW} -X POST --header 'Content-Type: application/json' -d '{ "name": "example-space-'"${num}"'" }' "${ENDPOINT}"'/spaces' --insecure
+for num in {1..${num_spaces}}; do
+  curl -s -D - --user noracle-example-smith:${agent_pw} -X POST --header 'Content-Type: application/json' -d '{ "name": "example-space-'"${num}"'" }' "${endpoint}"'/spaces' --insecure
 done
 
 
-# get all subscribed for agent smith
+# get all subscribed spaces for agent smith
 
-curl -s --user noracle-example-smith:${AGENT_PW} -X GET --header 'Accept: application/json' "${ENDPOINT}"'/agents/'"${SPACE_CREATOR_AGENT_ID}"'/spacesubscriptions' --insecure | jq -r '.[].spaceId' | while read spaceId ; do
-  for num in {1..50}; do
+out="$( { curl -s --user noracle-example-smith:${agent_pw} -X GET --header 'Accept: application/json' "${endpoint}"'/agents/'"${space_creator_agent_id}"'/spacesubscriptions' --insecure ; } )"
+echo "${out}" | jq -r '.[].spaceId' | while read spaceId ; do
+  # create questions inside space
+  question_ids=()
+  for num in {1..${num_questions_per_space}}; do
     random=$(random_lengthLorem)
-    curl -s -D - --user noracle-example-smith:${AGENT_PW} -X POST --header 'Content-Type: application/json' -d '{ "text": "'"${random}"'" }' "${ENDPOINT}"'/spaces/'"${spaceId}"'/questions' --insecure
+    out="$( { curl -s -D - --user noracle-example-smith:"${agent_pw}" -X POST --header 'Content-Type: application/json' -d '{ "text": "'"${random}"'" }' "${endpoint}"'/spaces/'"${spaceId}"'/questions' --insecure ; } 2>&1 )"
+    echo "${out}"
+    location_header="$( echo "${out}" | tr -d '\r' | sed -En 's/^Location: (.*)/\1/p' )"
+    question_id=${location_header##*/}
+    question_ids+=(${question_id})
+  done
+  # create relations between questions
+  for num in {1..${num_relations_per_space}}; do
+    relation_type=${relation_types[$RANDOM % ${#relation_types[@]} ]}
+    first_question_id=${question_ids[$RANDOM % ${#question_ids[@]} ]}
+    second_question_id=${question_ids[$RANDOM % ${#question_ids[@]} ]}
+    echo "relating question ${first_question_id} to ${second_question_id} as ${relation_type}"
+    out="$( { curl -s -D - --user noracle-example-smith:"${agent_pw}" -X POST --header 'Content-Type: application/json' -d '{ '"${relation_type}"', "firstQuestionId": "'"${first_question_id}"'", "secondQuestionId": "'"${second_question_id}"'" }' "${endpoint}"'/spaces/'"${spaceId}"'/relations' --insecure ; } 2>&1 )"
+    echo "${out}"
   done
 done
+
