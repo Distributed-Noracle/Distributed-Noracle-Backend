8b27fce36de74b7c528001108da264a7d1e44d03
Jan Benscheid <jan.benscheid@gmail.com>
2018-03-26 12:07:41 +0200

    API for retrieving space members

--- src/main/i5/las2peer/services/noracleService/api/INoracleSpaceService.java
+++ src/main/i5/las2peer/services/noracleService/api/INoracleSpaceService.java
@@ -2,11 +2,14 @@ package i5.las2peer.services.noracleService.api;
 
 import i5.las2peer.api.execution.ServiceInvocationException;
 import i5.las2peer.services.noracleService.model.Space;
+import i5.las2peer.services.noracleService.model.SpaceSubscribersList;
 
 public interface INoracleSpaceService {
 
 	public Space createSpace(String name) throws ServiceInvocationException;
 
 	public Space getSpace(String spaceId) throws ServiceInvocationException;
+	
+	public SpaceSubscribersList getSubscribers(String spaceId) throws ServiceInvocationException;
 
 }
---
+++ src/main/i5/las2peer/services/noracleService/model/SpaceSubscribersList.java
@@ -0,0 +1,9 @@
+package i5.las2peer.services.noracleService.model;
+
+import java.util.ArrayList;
+
+public class SpaceSubscribersList extends ArrayList<NoracleAgentProfile> {
+
+	private static final long serialVersionUID = 1L;
+	
+}
