9dac4042c2fe6d80b0a4f1d6931d6cdb798ba747
Timo Bergerbusch <timo.bergerbusch@web.de>
2019-10-30 15:11:12 +0100

    Remove Preprocessing Services

--- src/main/i5/las2peer/services/noracleService/api/INoraclePreprocessingService.java
+++
@@ -1,8 +0,0 @@
-package i5.las2peer.services.noracleService.api;
-
-import i5.las2peer.api.execution.ServiceInvocationException;
-
-public interface INoraclePreprocessingService {
-
-	public void checkStatus() throws ServiceInvocationException;
-}
